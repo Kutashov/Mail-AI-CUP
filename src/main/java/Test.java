9dcb8c88d75299fcdf5c2deb085b29814d9bd866
AlexandrKutashov <kutashov.alexandr@yandex.ru>
2012-11-03 13:18:38 +0400

    Renovated test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,13 +3,14 @@ import java.io.IOException;
 
 public class Test {
 
+	static String runner_path = "C:/workspace/TankWars/local-runner.bat";
 	/**
 	 * @param args
 	 * @throws IOException 
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws IOException, InterruptedException {
-		Process p = Runtime.getRuntime().exec("C:/Documents and Settings/÷»‘–ŒÕŒÃ/workspace/TankWars/local-runner.bat");
+		Process p = Runtime.getRuntime().exec(runner_path);
 		p.waitFor();
 		Runner.main(args);
 
